83929ef9c75960cfa8de58610b7214030977da07
Cillian <contact@cilliano.com>
2010-11-25 12:04:07 +0000

    update_all not working

--- db/migrate/20101125110554_add_consumption_type_to_foods.rb
+++ db/migrate/20101125110554_add_consumption_type_to_foods.rb
@@ -2,7 +2,7 @@ class AddConsumptionTypeToFoods < ActiveRecord::Migration
   def self.up
     add_column :foods, :consumption_type, :string
     Food.reset_column_information
-    Food.update_all('consumption_type = "food"')
+    Food.update_all("consumption_type = 'food'")
   end
 
   def self.down
