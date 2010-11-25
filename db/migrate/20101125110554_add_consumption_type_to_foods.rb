286f76ef8694fcb94a3cdd26bde345287ce0f8de
Cillian <contact@cilliano.com>
2010-11-25 11:56:24 +0000

    forgot to reset column information in migration

--- db/migrate/20101125110554_add_consumption_type_to_foods.rb
+++ db/migrate/20101125110554_add_consumption_type_to_foods.rb
@@ -1,6 +1,7 @@
 class AddConsumptionTypeToFoods < ActiveRecord::Migration
   def self.up
     add_column :foods, :consumption_type, :string
+    Food.reset_column_information
     Food.update_all('consumption_type = "food"')
   end
 
